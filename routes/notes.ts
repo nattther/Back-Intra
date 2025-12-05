4f0b7856543358e8955d91b2d74e5a1eaa494e4a
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2025-12-05 10:29:33 +0100

    Update notes.ts

--- routes/notes.ts
+++ routes/notes.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // ⚠️ route spécifique AVANT ou APRÈS, peu importe ici,
 // "/" ne matche que exactement "/"
-//router.get("/student/:userId", getNotesByStudent);
+router.get("/student/:userId", getNotesByStudent);
 
 router.get("/", getNotes);
 router.post("/", addNote);
